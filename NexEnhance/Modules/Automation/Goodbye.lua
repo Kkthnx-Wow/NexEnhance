25da0bbfebe77074ed7636e69a4546d2aac31840
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2024-11-27 12:15:03 -0500

    Defer the goodbye

--- NexEnhance/Modules/Automation/Goodbye.lua
+++ NexEnhance/Modules/Automation/Goodbye.lua
@@ -15,14 +15,11 @@ local AutoThanksList = {
 local function SendAutoGoodbyeMessage()
 	local message
 	if Module.db.profile.automation.CustomGoodbyeMessage and Module.db.profile.automation.CustomGoodbyeMessage ~= "" then
-		-- Use the custom message if set
 		message = Module.db.profile.automation.CustomGoodbyeMessage
 	else
-		-- Use a random message from the list
 		message = AutoThanksList[math_random(#AutoThanksList)]
 	end
 
-	-- Determine the chat channel
 	local channel
 	if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
 		channel = "INSTANCE_CHAT"
@@ -32,7 +29,6 @@ local function SendAutoGoodbyeMessage()
 		channel = "SAY"
 	end
 
-	-- Send the message
 	if message then
 		SendChatMessage(message, channel)
 	end
@@ -40,17 +36,17 @@ end
 
 -- Setup delayed goodbye message
 local function SetupAutoGoodbye()
-	C_Timer.After(math_random(2, 5), SendAutoGoodbyeMessage)
+	Module:Defer(function()
+		C_Timer.After(math_random(2, 5), SendAutoGoodbyeMessage)
+	end)
 end
 
 -- Create or disable Auto Goodbye feature
 function Module:PLAYER_LOGIN()
 	if Module.db.profile.automation.AutoGoodbye then
-		-- Register events to trigger the goodbye message
 		Module:RegisterEvent("LFG_COMPLETION_REWARD", SetupAutoGoodbye)
 		Module:RegisterEvent("CHALLENGE_MODE_COMPLETED", SetupAutoGoodbye)
 	else
-		-- Unregister events when the feature is disabled
 		Module:UnregisterEvent("LFG_COMPLETION_REWARD", SetupAutoGoodbye)
 		Module:UnregisterEvent("CHALLENGE_MODE_COMPLETED", SetupAutoGoodbye)
 	end
